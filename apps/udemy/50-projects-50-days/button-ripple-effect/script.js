fdfdddf676d22e7959cec93134189d24b6d4bcef
eGaruru <162013145+eGaruru@users.noreply.github.com>
2026-04-22 22:21:48 +0200

    feat(button-ripple-effect): adapt random color to ripple effect

--- apps/udemy/50-projects-50-days/button-ripple-effect/script.js
+++ apps/udemy/50-projects-50-days/button-ripple-effect/script.js
@@ -1,4 +1,13 @@
-// To adapt same effect for multipale buttons
+const colors = [
+  'rgb(255, 255, 255)',
+  'rgb(191, 255, 0)',
+  'rgb(0, 255, 255)',
+  'rgb(255, 0, 255)',
+  'rgb(255, 235, 59)',
+  'rgb(255, 165, 0)',
+];
+
+// To adapt same effect for multiple buttons
 const buttons = document.querySelectorAll('.ripple');
 
 buttons.forEach((button) => {
@@ -17,8 +26,23 @@ buttons.forEach((button) => {
     circle.style.top = yInside + 'px';
     circle.style.left = xInside + 'px';
 
+    const randomColor = colors[getRandomIndex(colors.length)];
+
+    // Sometimes a color similar to purple appears
+    // const randomColor = `rgb(${getRandomNumber(0, 255)},${getRandomNumber(0, 255)},${getRandomNumber(0, 255)})`;
+
+    circle.style.backgroundColor = randomColor;
+
     this.appendChild(circle);
 
     setTimeout(() => circle.remove(), 500);
   });
 });
+
+function getRandomIndex(length) {
+  return Math.floor(Math.random() * length);
+}
+
+// function getRandomNumber(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// }

44aa257363487c38589084c0ebf66fe3b7088f63
eGaruru <162013145+eGaruru@users.noreply.github.com>
2026-05-02 22:59:32 +0200

    feat(double-vertical-slider): implement infinite loop transition

--- apps/udemy/50-projects-50-days/double-vertical-slider/script.js
+++ apps/udemy/50-projects-50-days/double-vertical-slider/script.js
@@ -3,30 +3,61 @@ const sliderRight = document.querySelector('.right-slide');
 const sliderLeft = document.querySelector('.left-slide');
 const upButton = document.querySelector('.up-button');
 const downButton = document.querySelector('.down-button');
-const slidesLength = sliderRight.querySelectorAll('div').length;
 
-let activeSlideIndex = 0;
+const slidesLength = sliderLeft.querySelectorAll('div').length;
 
-sliderLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
+// create clones and add in slides
+const firstRightClone = sliderRight.firstElementChild.cloneNode(true);
+const lastReightClone = sliderRight.lastElementChild.cloneNode(true);
+const firstLeftClone = sliderLeft.firstElementChild.cloneNode(true);
+const lastLeftClone = sliderLeft.lastElementChild.cloneNode(true);
+
+sliderRight.appendChild(firstRightClone);
+sliderRight.prepend(lastReightClone);
+sliderLeft.appendChild(firstLeftClone);
+sliderLeft.prepend(lastLeftClone);
+
+let activeSlideIndex = 1;
+
+// initial setting
+sliderLeft.style.top = `-${(slidesLength + 1) * 100}vh`;
+
+calculateDisplayHeight();
 
 upButton.addEventListener('click', () => changeSlide('up'));
 downButton.addEventListener('click', () => changeSlide('down'));
 
 const changeSlide = (direction) => {
-  const sliderHeight = sliderContainer.clientHeight;
+  sliderLeft.style.transition = 'transform 0.5s ease-in-out';
+  sliderRight.style.transition = 'transform 0.5s ease-in-out';
+
   if (direction === 'up') {
     activeSlideIndex++;
-    if (activeSlideIndex > slidesLength - 1) {
-      activeSlideIndex = 0;
-    }
   } else if (direction === 'down') {
     activeSlideIndex--;
-    if (activeSlideIndex < 0) {
-      activeSlideIndex = slidesLength - 1;
-    }
   }
+
+  calculateDisplayHeight();
+};
+
+sliderRight.addEventListener('transitionend', () => {
+  if (activeSlideIndex === slidesLength + 1) {
+    sliderLeft.style.transition = 'none';
+    sliderRight.style.transition = 'none';
+    activeSlideIndex = 1;
+  } else if (activeSlideIndex === 0) {
+    sliderLeft.style.transition = 'none';
+    sliderRight.style.transition = 'none';
+    activeSlideIndex = slidesLength;
+  }
+
+  calculateDisplayHeight();
+});
+
+function calculateDisplayHeight() {
+  const sliderHeight = sliderContainer.clientHeight;
 
   sliderRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`;
 
   sliderLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`;
-};
+}

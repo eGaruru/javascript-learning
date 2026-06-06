e1841eb69ef5fbd10a64754362112a8a89e6aba5
eGaruru <162013145+eGaruru@users.noreply.github.com>
2026-06-06 23:59:27 +0200

    【WIP】feat(exchange-rate-calculator): add project files

---
+++ apps/udemy/20-web-projects-with-vanilla-javascript/exchange-rate-calculator/script.js
@@ -0,0 +1,32 @@
+const currencyEl_one = document.getElementById('currency-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_one = document.getElementById('amount-one');
+const amountEl_two = document.getElementById('amount-two');
+
+const rateEl = document.getElementById('rate');
+const swapEl = document.getElementById('swap');
+
+// Fetch exchange rates and update the DOM
+function calculate() {
+  const currency_one = currencyEl_one.value;
+  const currency_two = currencyEl_two.value;
+
+  fetch(`https://open.exchangerate-api.com/v6/latest`)
+    .then((res) => res.json())
+    .then((data) => {
+      // console.log(data);
+      const rate = data.rates[currency_two] / data.rates[currency_one];
+
+      rateEl.innerText = ``;
+    });
+}
+
+// Event listeners
+currencyEl_one.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_two.addEventListener('input', calculate);
+
+calculate();
+
+// https://api.exchangeratesapi.io/latest?base=${currency_one}
